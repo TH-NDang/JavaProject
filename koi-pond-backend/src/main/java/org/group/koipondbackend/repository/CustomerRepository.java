f4cf40598d170f4b95db33ee91b4cb673effeae9
ndang <ndang2319@gmail.com>
2024-10-27 02:30:23 +0700

    Remove Customer and Manager related classes and services to simplify the backend

--- koi-pond-backend/src/main/java/org/group/koipondbackend/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package org.group.koipondbackend.repository;
-
-import org.group.koipondbackend.entity.Customer;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends UserRepository<Customer> {
-}
