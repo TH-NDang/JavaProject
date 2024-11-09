fd6206c965880d048d903c16fdb697de168a3057
ndang <ndang2319@gmail.com>
2024-11-09 21:15:10 +0700

    Refactor OrderMapper and ServiceMapper: streamline mapping annotations and improve readability

--- koi-pond-backend/src/main/java/org/group/koipondbackend/mapper/ServiceMapper.java
+++ koi-pond-backend/src/main/java/org/group/koipondbackend/mapper/ServiceMapper.java
@@ -4,23 +4,25 @@ import org.group.koipondbackend.dto.service.ServiceDTO;
 import org.group.koipondbackend.dto.service.CreateServiceRequest;
 import org.group.koipondbackend.dto.service.UpdateServiceRequest;
 import org.group.koipondbackend.entity.Services;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
-@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Mapper(componentModel = "spring")
 public interface ServiceMapper {
+
     @Mapping(target = "id", source = "id")
     @Mapping(target = "name", source = "name")
     @Mapping(target = "description", source = "description")
     @Mapping(target = "price", source = "price")
+    @Mapping(target = "features", source = "features")
+    @Mapping(target = "isPopular", source = "isPopular")
     ServiceDTO toDto(Services services);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "name", source = "name")
     @Mapping(target = "description", source = "description")
     @Mapping(target = "price", source = "price")
+    @Mapping(target = "features", source = "features")
+    @Mapping(target = "isPopular", source = "isPopular")
     Services toEntity(CreateServiceRequest request);
 
     @Mapping(target = "id", ignore = true)
